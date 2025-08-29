7854da5eb9c3bcc90a469df8d9c1ce87fdad5034
Gowtham <gowtham7544gowda@gmail.com>
2025-08-29 10:33:33 +0530

    A simple frontend is added

--- app.py
+++ app.py
@@ -17,12 +17,22 @@ def home():
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data=request.json['data']
-    print(data)
-    print(np.array(list(data.values())).reshape(1,-1))
+    # print(data)
+    # print(np.array(list(data.values())).reshape(1,-1))
     new_data=Scalermodel.transform(np.array(list(data.values())).reshape(1,-1))
     output=model.predict(new_data)
-    print(output[0])
+    # print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=Scalermodel.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=model.predict(final_input)[0]
+    return render_template("home.html",prediction_text="{}".format(output))
+    
+    
 
 if __name__=="__main__":
     app.run(debug=True)
